2eb6e57149928eef2eabe2c26e12ebc79d45bd37
sarikabhatta <sarikabhatta5@gmail.com>
2024-12-29 15:59:21 +0545

    create food items

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,42 @@
-import Container from '@/components/Container';
-import Explore from '@/components/Explore';
-import Burger from '@/components/icons/burger';
-import Location from '@/components/icons/Location';
-import JiffyLogo from '@/components/JiffyLogo';
-import Navbar from '@/components/Navbar';
-import Image from 'next/image';
+import Container from "@/components/Container";
+import Explore from "@/components/Explore";
+import FoodItems from "@/components/FoodItems";
+import Burger from "@/components/icons/burger";
+import Location from "@/components/icons/Location";
+import JiffyLogo from "@/components/JiffyLogo";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
-	return (
-		<div className='bg-background max-w-[430px] mx-auto'>
-			<Container>
-				<div className='flex justify-between items-center'>
-					<div>
-						<JiffyLogo />
-					</div>
-					<div>
-						<Burger />
-					</div>
-				</div>
-				<div className='flex items-center gap-x-1'>
-					<div>
-						<Location />
-					</div>
-					<h1 className='text-xs text-text-secondary'>
-						The Gardens, Panipokhari, Maharajgunj, Kathmandu
-					</h1>
-				</div>
-				<div className='py-2'>
-					<Explore />
-				</div>
-			</Container>
-			<div className='sticky bottom-0'>
-				<Navbar />
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-background max-w-[430px] mx-auto">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div>
+            <JiffyLogo />
+          </div>
+          <div>
+            <Burger />
+          </div>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <div>
+            <Location />
+          </div>
+          <h1 className="text-xs text-text-secondary">
+            The Gardens, Panipokhari, Maharajgunj, Kathmandu
+          </h1>
+        </div>
+        <div>
+          <FoodItems />
+        </div>
+        <div className="py-2">
+          <Explore />
+        </div>
+      </Container>
+      <div className="sticky bottom-0">
+        <Navbar />
+      </div>
+    </div>
+  );
 }
--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -2,10 +2,37 @@ import React from 'react'
 
 const FoodItems = () => {
   return (
-    <div>
-        <div>
-            <img src='image2.png' alt='food-icon'></img>
+    <div className='flex items-center gap-x-3  mt-10'>
+       
+        <div className='flex flex-col gap-y-1 items-center'>
+          <div>
+            <img src="/images/image5.png" className='h-12' alt="" />
+          </div>
+          <div>Pizza</div>
         </div>
+        <div className='flex flex-col gap-y-1 items-center'>
+          <div>
+            <img src="/images/image4.png" className='h-14' alt="" />
+          </div>
+          <div>Birayani</div>
+        </div>
+
+        <div className='flex flex-col gap-y-1 items-center'>
+          <div>
+            <img src="/images/image4.png" className='h-14' alt="" />
+          </div>
+          <div>Birayani</div>
+        </div>
+        <div className='flex flex-col gap-y-1 items-center'>
+          <div>
+            <img src="/images/image4.png" className='h-14' alt="" />
+          </div>
+          <div>Birayani</div>
+        </div>
+
+     
+
+        
     </div>
   )
 }
