e2274c9c0b9e61b53c60a5b7af19d97a5b3bb158
Samip Poudel <sawmeep58@gmail.com>
2024-12-29 12:14:17 +0545

    chore: setup color variables

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
 export default function Home() {
-	return <div className='primary '>Hello world!</div>;
+  return (
+    <div className="primary ">
+      <div>
+        <div className="h-16 w-16 text-background bg-brand">Hello</div>
+      </div>
+    </div>
+  );
 }
