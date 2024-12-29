f0e1e454ea82b9b8cf52fcc51863a63ccf572798
sarikabhatta <sarikabhatta5@gmail.com>
2024-12-29 15:21:02 +0545

    create food-items

---
+++ src/components/FoodItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FoodItems = () => {
+  return (
+    <div>
+        <div>
+            <img src='image2.png' alt='food-icon'></img>
+        </div>
+    </div>
+  )
+}
+
+export default FoodItems
