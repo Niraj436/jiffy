aaa079db9924be76691e3355492a7afa90a66447
niraj436 <nirajkhadka634@gmail.com>
2024-12-30 15:04:26 +0545

    seed data

--- backend/data/restaurant.js
+++ backend/data/restaurant.js
@@ -1,40 +1,204 @@
 const seedRestaurants = [
-  {
-    name: 'The Gourmet Kitchen',
-    image: '/images/image1.jpg',
-    location: '123 Foodie Lane, New York, NY',
-    rating: 4.5,
-  },
-  {
-    name: 'Ocean Delight',
-    image: '/images/image.jpg',
-    location: '456 Seaside Blvd, Miami, FL',
-    rating: 4.7,
-  },
-  {
-    name: 'Mountain Feast',
-    image: '/images/image-3.jpg',
-    location: '789 Summit Ave, Denver, CO',
-    rating: 4.3,
-  },
-  {
-    name: 'Urban Bites',
-    image: '/images/image-2.jpg',
-    location: '321 Metro St, Los Angeles, CA',
-    rating: 4.6,
-  },
-  {
-    name: 'Country Comforts',
-    image: '/images/image-3.jpg',
-    location: '654 Rustic Rd, Austin, TX',
-    rating: 4.4,
-  },
-  {
-    name: 'Chulesi Restro',
-    image: '/images/image-3.jpg',
-    location: '654 Rustic Rd, Austin, TX',
-    rating: 4.3,
-  },
+	{
+		name: 'Paalcha Kitchen',
+		image: '/images/image1.jpg',
+		location: 'Maharajgunj, Kathmandu',
+		rating: 3.9,
+	},
+	{
+		name: 'Maicha Khaja Ghar',
+		image: '/images/image.jpg',
+		location: 'Ason, Maha Boudha, Kathmandu',
+		rating: 3.2,
+	},
+	{
+		name: 'Sa Sa Twa',
+		image: '/images/image-3.jpg',
+		location: 'Samipchowk, Kritipur',
+		rating: 4.2,
+	},
+	{
+		name: 'Paleti Bhansa Ghar',
+		image: '/images/image-2.jpg',
+		location: 'SanoThimi, Bhatakpur',
+		rating: 4.9,
+	},
+	{
+		name: 'Harisiddhi Newa Suhlee',
+		image: '/images/image-3.jpg',
+		location: 'Thamel, Kathmandu',
+		rating: 4.4,
+	},
+	{
+		name: 'Chulesi Restro',
+		image: '/images/image-3.jpg',
+		location: 'Budanilkantha, Kathmandu',
+		rating: 4.3,
+	},
+	{
+		name: 'Newa Naani',
+		image: '/images/image-3.jpg',
+		location: 'Boudha, Kathmandu',
+		rating: 4.1,
+	},
+
+	{
+		name: 'Muskan Paasa Restro',
+		image: '/images/image-3.jpg',
+		location: 'Chabahil, Kathmandu',
+		rating: 4.4,
+	},
+
+	{
+		name: 'Mithila Thali',
+		image: '/images/image-3.jpg',
+		location: 'Jhamsikhel, Lalitpur',
+		rating: 4.4,
+	},
+
+	{
+		name: 'Mithila Aagan',
+		image: '/images/image-3.jpg',
+		location: 'Chabahil, Kathmandu',
+		rating: 4.7,
+	},
+
+	{
+		name: 'House of Thalis',
+		image: '/images/image-3.jpg',
+		location: 'Gaushala, Kathmandu',
+		rating: 4.5,
+	},
+
+	{
+		name: 'Janaki Garden',
+		image: '/images/image-3.jpg',
+		location: 'Kharibot, Kathmandu',
+		rating: 4.5,
+	},
+
+	{
+		name: 'Bimal Kesari Restro',
+		image: '/images/image-3.jpg',
+		location: 'Raaniban, Kathmandu',
+		rating: 4.9,
+	},
+	{
+		name: 'Aema K',
+		image: '/images/image-3.jpg',
+		location: 'Boudha, Kathmandu',
+		rating: 4.4,
+	},
+
+	{
+		name: 'Smile Chinese Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Raniban, Kathmandu',
+		rating: 4.8,
+	},
+
+	{
+		name: 'Little Tibet Restaurant & Bar',
+		image: '/images/image-3.jpg',
+		location: 'Narsingh Chowk Marg, Kathmandu ',
+		rating: 4.3,
+	},
+
+	{
+		name: 'Tibetan Kitchen',
+		image: '/images/image-3.jpg',
+		location: 'Raniban, Kathmandu',
+		rating: 4.6,
+	},
+
+	{
+		name: 'Tibet Cafe',
+		image: '/images/image-3.jpg',
+		location: 'Thamel, Kathmandu',
+		rating: 3.8,
+	},
+
+	{
+		name: 'Yangling Tibetan Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Kaldhara Marg, Kathmandu',
+		rating: 3.9,
+	},
+
+	{
+		name: 'Dechenling Garden Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Keshar Mahal Marg, Kathmandu',
+		rating: 4.2,
+	},
+	{
+		name: 'Shambhala Café',
+		image: '/images/image-3.jpg',
+		location: 'Boudha, Kathmandu',
+		rating: 4.1,
+	},
+	{
+		name: 'Gawa Resturant',
+		image: '/images/image-3.jpg',
+		location: 'Keshar Mahal Marg, Kathmandu',
+		rating: 4.2,
+	},
+	{
+		name: 'Tashi Delek Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Jhamsikhel, Kathmandu',
+		rating: 3.5,
+	},
+	{
+		name: 'DKK Taste of Sikkim',
+		image: '/images/image-3.jpg',
+		location: 'Amrit Marg, Kathmandu',
+		rating: 3.2,
+	},
+	{
+		name: 'Utpala Cafe',
+		image: '/images/image-3.jpg',
+		location: 'Boudha, Kathmandu',
+		rating: 3.9,
+	},
+
+	{
+		name: 'Organic Smoothie Bowl and Cafe',
+		image: '/images/image-3.jpg',
+		location: 'Boudha, Kathmandu',
+		rating: 4.2,
+	},
+	{
+		name: 'Loving Heart Vegan Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Chaksibari Marg, Kathmandu',
+		rating: 4.5,
+	},
+	{
+		name: 'Yala Cafe & Coffee Shop',
+		image: '/images/image-3.jpg',
+		location: 'Thamel Marg, Kathmandu',
+		rating: 4.7,
+	},
+	{
+		name: 'Rosemary Kitchen and Coffee Shop',
+		image: '/images/image-3.jpg',
+		location: 'JP Marg, Kathmandu',
+		rating: 4.4,
+	},
+	{
+		name: 'Annapurna Veg and Vegan Restaurant',
+		image: '/images/image-3.jpg',
+		location: 'Thamel Marg, Kathmandu',
+		rating: 4.1,
+	},
+	{
+		name: 'Revolution Cafe',
+		image: '/images/image-3.jpg',
+		location: 'Naya Sadak, Kathmandu',
+		rating: 4.2,
+	},
 ];
+
 
 export default seedRestaurants;
