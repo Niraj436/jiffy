d5835f1cc6cb285d17fc9a8b7f938ac1e361e32a
niraj436 <nirajkhadka634@gmail.com>
2024-12-31 04:53:24 +0545

    removed burger in home

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Container from '@/components/Container';
 import Explore from '@/components/Explore';
 import FoodItems from '@/components/FoodItems';
 import Header from '@/components/Header';
-import Burger from '@/components/icons/burger';
+
 import Location from '@/components/icons/Location';
 import JiffyLogo from '@/components/JiffyLogo';
 import LocationBar from '@/components/LocationBar';
@@ -15,32 +15,32 @@ import useGetRestaurants from '@/hooks/useGetRestaurants';
 import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <>
-      <div className='bg-background max-w-[430px] mx-auto'>
-        <Container>
-          <Header />
-          <LocationBar />
+	return (
+		<>
+			<div className='bg-background max-w-[430px] mx-auto'>
+				<Container>
+					<Header />
+					<LocationBar />
 
-          <div className='pt-5'>
-            <Search />
-          </div>
-          <div className='pt-5'>
-            <FoodItems />
-          </div>
-          <div className='pt-3'>
-            <Link href={'/quests'}>
-              <QuestBanner />
-            </Link>
-          </div>
-          <div className='py-2'>
-            <Explore />
-          </div>
-        </Container>
-        <div className='sticky bottom-0 bg-background'>
-          <Navbar />
-        </div>
-      </div>
-    </>
-  );
+					<div className='pt-5'>
+						<Search />
+					</div>
+					<div className='pt-5'>
+						<FoodItems />
+					</div>
+					<div className='pt-3'>
+						<Link href={'/quests'}>
+							<QuestBanner />
+						</Link>
+					</div>
+					<div className='py-2'>
+						<Explore />
+					</div>
+				</Container>
+				<div className='sticky bottom-0 bg-background'>
+					<Navbar />
+				</div>
+			</div>
+		</>
+	);
 }
